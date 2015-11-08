3b5bd3a03980764f35633be855c9f3dc6685dbd5
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-11-09 00:42:57 +0200

    init some files

---
+++ src/QueryBuilder/Query.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Project: wp-query-builder
+ * File: Query.php
+ * User: Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * Date: 9/11/2015
+ * Time: 12:41 πμ
+ * Since: TODO ${VERSION}
+ * Copyright: 2015 Panagiotis Vagenas
+ */
+
+namespace Pan\QueryBuilder;
+
+
+class Query {
+
+}
