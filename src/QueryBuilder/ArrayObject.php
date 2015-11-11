989794933c320d1cf9e918f05a08e4988a4515b7
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-11-11 22:09:05 +0200

    Overriding some ArrayObject methods in order to work with our schema

--- src/QueryBuilder/ArrayObject.php
+++ src/QueryBuilder/ArrayObject.php
@@ -12,6 +12,50 @@
 namespace Pan\QueryBuilder;
 
 
+/**
+ * Class ArrayObject
+ *
+ * @package Pan\QueryBuilder
+ * @author  Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * @since   TODO ${VERSION}
+ */
 class ArrayObject extends \ArrayObject {
+	/**
+	 * ArrayObject constructor.
+	 *
+	 * @param array  $input
+	 * @param int    $flags
+	 * @param string $iterator_class
+	 *
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function __construct( $input = array(), $flags = 3, $iterator_class = "ArrayIterator" ) {
+		parent::__construct( $input, $flags, $iterator_class );
+	}
 
+	/**
+	 * @param mixed $data
+	 *
+	 * @return array
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function exchangeArray( $data ) {
+		foreach ( $this->getArrayCopy() as $index => $item ) {
+			if ( isset( $data[ $index ] ) ) {
+				$this->{$index} = $data;
+			}
+		}
+		return $this->getArrayCopy();
+	}
+
+	/**
+	 * @return array
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function getArrayCopy() {
+		return get_object_vars( $this );
+	}
 }
