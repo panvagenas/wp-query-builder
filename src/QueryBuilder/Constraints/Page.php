f3cf69cb71fc8b63c4728169c13f4c075dc1f9f9
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-11-15 19:17:40 +0200

    Wrong parent

--- src/QueryBuilder/Constraints/Page.php
+++ src/QueryBuilder/Constraints/Page.php
@@ -11,6 +11,8 @@
 
 namespace Pan\QueryBuilder\Constraints;
 
+use Pan\QueryBuilder\Constraints\Abs\PostPage;
+
 /**
  * {@inheritdoc}
  *
@@ -18,7 +20,7 @@ namespace Pan\QueryBuilder\Constraints;
  * @author  Panagiotis Vagenas <pan.vagenas@gmail.com>
  * @since   TODO ${VERSION}
  */
-class Page extends Post {
+class Page extends PostPage {
     /**
      * use page id
      *
