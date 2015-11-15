d12f7e1b343a7bde69a3ac8a5aa8e3711d968938
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-11-15 16:35:03 +0200

    More tests

--- tests/Constraints/PostTest.php
+++ tests/Constraints/PostTest.php
@@ -43,4 +43,35 @@ class PostTest extends QueryBuilderUnitTestCase {
         $this->assertEquals(1, $wpQ->found_posts);
         $this->assertContains($wpPost->ID, wp_list_pluck($wpQ->posts, 'ID'));
     }
+    public function testQueeringByParent(){
+        /** @var \WP_Post $wpPost1 */
+        $wpPost1 = $this->factory()->post->create_and_get();
+        /** @var \WP_Post $wpPost1 */
+        $wpPost2 = $this->factory()->post->create_and_get();
+
+        $numOfPostWithParentP1 = 5;
+        $post1Children = $this->factory()->post->create_many($numOfPostWithParentP1, array('post_parent' => $wpPost1->ID));
+
+        $numOfPostWithParentP2 = 6;
+        $post2Children = $this->factory()->post->create_many($numOfPostWithParentP2, array('post_parent' => $wpPost2->ID));
+
+        $post = new Post();
+        $post->setPostParent($wpPost1->ID);
+
+        $q1 = $post->crtAndAddBuilder()->crtQuery();
+        $wpQ = $q1->getResult();
+        $q1->sort($post1Children);
+
+        $this->assertEquals($numOfPostWithParentP1, $wpQ->found_posts);
+        $this->assertArraySubset($post1Children, wp_list_pluck($wpQ->posts, 'ID'));
+
+        $post->setPostParent($wpPost2->ID);
+
+        $q2 = $post->crtAndAddBuilder()->crtQuery();
+        $wpQ = $q2->getResult();
+        $q2->sort($post2Children);
+
+        $this->assertEquals($numOfPostWithParentP2, $wpQ->found_posts);
+        $this->assertArraySubset($post2Children, wp_list_pluck($wpQ->posts, 'ID'));
+    }
 }
